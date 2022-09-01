a80b0b8f6a32ed1d18ac55cc8b53558c650baab0
newObjectccc <897333949@qq.com>
2022-09-01 12:07:46 +0800

    feat(_util): 增加首字符大小写转换工具函数

--- src/_util/string/index.ts
+++ src/_util/string/index.ts
@@ -1,5 +1,10 @@
 // eslint-disable-next-line import/prefer-default-export
+
+// 首字符大写
 export function toFirstWordsUpper(str: string): string {
-  // return str.toLowerCase().replace(/( |^)[a-z]/g, (s) => s.toUpperCase())
   return str.replace(/( |^)[a-z]/g, (s) => s.toUpperCase());
+}
+// 只有首字符大写
+export function toFirstWordsUpperOnly(str: string): string {
+  return str.toLowerCase().replace(/( |^)[a-z]/g, (s) => s.toUpperCase());
 }
