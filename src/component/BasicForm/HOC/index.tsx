f42dd24d574dfbbbfaaa6f2480e90e22c5f5be79
newObjectccc <897333949@qq.com>
2022-09-19 19:41:32 +0800

    feat(BasicForm-HOC): 新增一个带提交和重置按钮的BasicFormHoC

--- src/component/BasicForm/HOC/index.tsx
+++ src/component/BasicForm/HOC/index.tsx
@@ -1,10 +1,47 @@
 import React from "react";
+import { Button } from "antd";
+import type { BasicFormPropsType } from "..";
+import locale from '../locale'
+import { useLocale } from '../../../hook'
 
-// eslint-disable-next-line import/prefer-default-export
-export const withConfirmBasicFormHoC = (basicForm: React.FC) => {
-  const withConfirmStatProps = {
-    children: 'xxx',
-  }
-  const ComponentWithStat = React.createElement(basicForm, withConfirmStatProps)
-  return ComponentWithStat
+export interface ConfirmBasicFormType extends BasicFormPropsType {
+  onSubmit?: <T extends any>(val: T) => T
+  onReset?: () => void
+  confirmWrapper?: React.ReactElement<any>
+  clsName?: string | (() => string) | string[] | {[key: string]: boolean}
+  [key: string]: any
 }
+
+
+const ConfirmGroup = ({onReset, form, confirmWrapper}: ConfirmBasicFormType) => {
+  const [textMap] = useLocale(locale)
+  // 支持自定义 Wrapper
+  if (!confirmWrapper) {
+    confirmWrapper = <div style={{display: 'flex'}} />
+  }
+  const confirmComponent = React.cloneElement(confirmWrapper, {}, 
+    <Button type="primary" htmlType="submit">{textMap.confirm}</Button>,
+    <Button style={{ margin: '0 8px' }} onClick={() => {
+      form?.resetFields?.()
+      onReset?.()
+    }}>{textMap.reset}</Button>,
+  )
+  return confirmComponent
+}
+
+
+export const withConfirmBasicFormHoC = (basicForm: React.FC<any>)  => (customProps: ConfirmBasicFormType) => {
+    const {onSubmit, onReset, form, confirmWrapper, formItemList = [], ...restProps} = customProps
+
+    const formItemListWithConfirm = [...formItemList, {
+      key: '#__confirm__',
+      // eslint-disable-next-line react/no-unstable-nested-components
+      customRender: () => <ConfirmGroup onReset={onReset} form={form} confirmWrapper={confirmWrapper} />,
+    }]
+
+    const onFinishHandler = React.useCallback((values: unknown) => {
+      onSubmit?.(values)
+    }, [onSubmit])
+
+    return React.createElement(basicForm, {...restProps, form, formItemList: formItemListWithConfirm, onFinish: onFinishHandler})
+  }
